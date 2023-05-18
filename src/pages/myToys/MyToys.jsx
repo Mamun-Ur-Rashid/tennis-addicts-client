3b27a2d819713e7fca56556d05a6d7d8a7c6bd95
Mamun-Ur-Rashid <engmamun656@gmail.com>
2023-05-19 01:38:59 +0600

    Update function added

--- src/pages/myToys/MyToys.jsx
+++ src/pages/myToys/MyToys.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../provider/AuthProvider';
 import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const MyToys = () => {
     const { user } = useContext(AuthContext);
@@ -14,6 +15,40 @@ const MyToys = () => {
                 setToys(data);
             })
     }, [user])
+
+    const handleDelete = (id) => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/toys/${id}`, {
+                    method: 'DELETE',
+                })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    if(data.deletedCount>0){
+                        Swal.fire(
+                            'Deleted!',
+                            'Your file has been deleted.',
+                            'success'
+                        )
+                        const remaining = toys.filter(toy => toy._id !== id);
+                        setToys(remaining);
+                    }
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+            }
+        })
+    }
     return (
         <div className='container my-16'>
             <h1 className='text-center text-3xl font-bold my-5'>MY Toys</h1>
@@ -37,23 +72,23 @@ const MyToys = () => {
                     </thead>
                     <tbody>
                         {
-                            toys?.map((toy, index) =>(  
-                            <tr key={toy._id}>
-                                <td>{index + 1}</td>
-                                <td>{toy.toyName}</td>
-                                <td><img className='h-14 w-24' src={toy.image} alt="" /></td>
-                                <td>{toy.sellerName}</td>
-                                <td>{toy.sellerEmail}</td>
-                                <td>{toy.category}</td>
-                                <td>{toy.price}</td>
-                                <td>{toy.rating}</td>
-                                <td>{toy.quantity}</td>
-                                <td><Link to={`/updateToy/${toy._id}`}><button>Update</button></Link></td>
-                                <td><button>Delete</button></td>
-                                <td>{toy.description}</td>
-                            </tr>))
+                            toys?.map((toy, index) => (
+                                <tr key={toy._id}>
+                                    <td>{index + 1}</td>
+                                    <td>{toy.toyName}</td>
+                                    <td><img className='h-14 w-24' src={toy.image} alt="" /></td>
+                                    <td>{toy.sellerName}</td>
+                                    <td>{toy.sellerEmail}</td>
+                                    <td>{toy.category}</td>
+                                    <td>{toy.price}</td>
+                                    <td>{toy.rating}</td>
+                                    <td>{toy.quantity}</td>
+                                    <td><Link to={`/updateToy/${toy._id}`}><button>Update</button></Link></td>
+                                    <td><button onClick={() => handleDelete(toy._id)}>Delete</button></td>
+                                    <td>{toy.description}</td>
+                                </tr>))
                         }
-                       
+
                     </tbody>
                 </table>
             </div>
